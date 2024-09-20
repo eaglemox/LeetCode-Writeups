0eab05c75af699b646fbf0539fff26a3fb9e0294
eaglemox <eagle627@gmail.com>
2024-09-21 03:44:33 +0800

    [214.shortest-palindrome] solution_3: KMP algorithm

--- string/214.shortest-palindrome/solution_3.c
+++ string/214.shortest-palindrome/solution_3.c
@@ -26,9 +26,31 @@ int* genNextTable(char* str) {
     // Find the longest prefix & suffix (LPS) of the pattern 
     int length = strlen(str);
     if (length == 0) {return NULL;}
+    int prevLen = 0; // record the previously found LPS length
     int *next = malloc(length * sizeof(int));
+    for (int i = 0; i < length; i++) {next[i] = 0;}
     next[0] = 0; // the only prefix, suffix is itself (str[0])
-    for 
+    for (int i = 1; i < length; ) { // until whole str is iterated
+        // imaging sliding a pattern below the string
+        // ababaca
+        //   xxx
+        //   ababaca
+        if (str[i] == str[prevLen]) { // if prefix == suffix 
+            prevLen++;
+            next[i] = prevLen;
+            i++;
+        } else { // if not matched
+            if (prevLen != 0) { // skip to the next char to re-match *move the pattern*
+                prevLen = next[prevLen - 1];
+            } else { // LPS length is zero, step forward
+                next[i] = 0;
+                i++;
+            }
+        }
+    }
+    // for (int i = 0; i < length; i++) {
+    //     printf("%d ", next[i]);
+    // } printf("\n");
     return next;
 }
 char* shortestPalindrome(char* s) {
@@ -37,25 +59,23 @@ char* shortestPalindrome(char* s) {
     if (length == 0) {return s;}
     
     char *reversed = reverse(s);
-    char *combined = malloc((2 * length + 2) * sizeof(char));
+    char *combined = malloc((2 * length + 2) * sizeof(char)); // add "#" and terminator
     combined[0] = '\0';
-    sprintf(combined, "%s#%s", s, reversed);
+
+    sprintf(combined, "%s#%s", s, reversed); // concate string into "s#s'" form 
+
     int *nextTable = genNextTable(combined);
-    int longestPalindromeLength = nextTable[strlen(combined) - 1];
-    char *result;
-    for (int i = 0; i < length; i++) {
-        if (!memcmp(s, reversed+i, length-i)) {
-            int appendLength = i;
-            result = malloc((length + appendLength + 1) * sizeof(char));
-            // reversed[0:i] substring
-            strncpy(result, reversed, appendLength); // do not contain '\0'
-            result[appendLength] = '\0';
-            strcat(result, s);
-            return result;
-        }
-    }
-    // dummy return
-    return s;
+    int appendLength = length - nextTable[strlen(combined) - 1];
+
+    char *result = malloc((length + appendLength + 1) * sizeof(char));
+    strncpy(result, reversed, appendLength);
+    result[appendLength] = '\0'; // set terminator
+    strcat(result, s);
+    
+    free(reversed);
+    free(combined);
+    free(nextTable);
+
+    return result;
 }
 // @lc code=end
-
