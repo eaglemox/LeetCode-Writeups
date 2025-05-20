1c9fea5250d2bd8317f83894e6fb9b9d91dd8d97
eaglemox <eagle627@gmail.com>
2025-05-20 12:08:55 +0800

    2025/5/20_daily

---
+++ array/3355.zero-array-transformation-i/solution_1.c
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=3355 lang=c
+ *
+ * [3355] Zero Array Transformation I
+ */
+
+// @lc code=start
+#include <stdbool.h>
+
+bool isZeroArray(int* nums, int numsSize, int** queries, int queriesSize, int* queriesColSize) {
+    // If a array is possible to be transformed into a zero array, each element must be less than the sum of queries covering its index.
+    int maxDecrement = 0;
+    int *range = malloc((numsSize + 1) * sizeof(int));
+    memset(range, 0, (numsSize + 1) * sizeof(int));
+
+    for (int i = 0; i < queriesSize; i++) {
+        range[queries[i][0]]++;
+        range[queries[i][1]+1]--; // r+1 is out of the range 
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        maxDecrement += range[i];
+        if (nums[i] > maxDecrement) {
+            return false;
+        }
+    }
+
+    return true;
+}
+// @lc code=end
+
