f34f0b557aaed894ae13e4e233c5abff63d18db9
eaglemox <eagle627@gmail.com>
2025-06-03 15:04:58 +0800

    2025/6/3_daily

---
+++ array/1298.maximum-candies-you-can-get-from-boxes/solution_2.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=1298 lang=cpp
+ *
+ * [1298] Maximum Candies You Can Get from Boxes
+ */
+
+// @lc code=start
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        // BFS to iterate all boxes we can open
+        int n = status.size();
+        // Obtained unopen boxes (boxes and keys are unique)
+        vector<bool> availableBox(n);
+        
+        queue<int> q;
+        for (int &box: initialBoxes) {
+            if (status[box]) {
+                q.push(box);
+            } else {
+                availableBox[box] = true;
+            }
+        }
+
+        int result = 0;
+        while (!q.empty()) {
+            // Open curr box
+            int curr = q.front();
+            q.pop();
+
+            result += candies[curr];
+            
+            // Open boxes with keys in current box
+            for (int &box: keys[curr]) {
+                status[box] = 1;
+                if (availableBox[box]) {
+                    availableBox[box] = false;
+                    q.push(box);
+                }
+            }
+
+            // Add boxes in current box into available boxes. 
+            // If we already have the key or the box is already open, we can push it immediately.
+            for (int &box: containedBoxes[curr]) {
+                if (status[box]) {
+                    q.push(box);
+                } else {
+                    availableBox[box] = true;
+                }
+            }
+
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
